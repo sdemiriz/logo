6a992d2b75dcf55417fa5fe994bb28331a672b6a
sedatDemiriz <sedatdemiriz97@gmail.com>
2024-01-04 22:49:04 -0800

    fix: correct minor/major curve variable names

--- logo.py
+++ logo.py
@@ -5,15 +5,15 @@ import nord as n
 d = dw.Drawing(500, 500, id_prefix="pic")
 d.append(dw.Rectangle(0, 0, 500, 500, fill=n.nord[1]))
 
-start_minor = (350, 350)
+start_major = (350, 350)
 size = (300, 300)
 copy_offset = (0, -100)
 offset = (0, -100)
 
 c.Curve(
     drawing=d,
-    start=start_minor,
-    end=(start_minor[0] + size[0], start_minor[1] + size[1]),
+    start=start_major,
+    end=(start_major[0] + size[0], start_major[1] + size[1]),
     offset=offset,
     height_offset=-50,
     color=n.nord[10],
@@ -22,10 +22,10 @@ c.Curve(
 
 c.Curve(
     drawing=d,
-    start=(start_minor[0] + copy_offset[0], start_minor[1] + copy_offset[1]),
+    start=(start_major[0] + copy_offset[0], start_major[1] + copy_offset[1]),
     end=(
-        start_minor[0] + size[0] + copy_offset[0],
-        start_minor[1] + size[1] + copy_offset[1],
+        start_major[0] + size[0] + copy_offset[0],
+        start_major[1] + size[1] + copy_offset[1],
     ),
     offset=offset,
     height_offset=-50,
@@ -33,14 +33,14 @@ c.Curve(
     debug=False,
 )
 
-start_major = (300, 300)
+start_minor = (300, 300)
 size = (400, -120)
 copy_offset = (0, 350)
 offset = (-25, 100)
 c.Curve(
     drawing=d,
-    start=start_major,
-    end=(start_major[0] + size[0], start_major[1] + size[1]),
+    start=start_minor,
+    end=(start_minor[0] + size[0], start_minor[1] + size[1]),
     offset=offset,
     height_offset=0,
     color=n.nord[9],
@@ -49,10 +49,10 @@ c.Curve(
 
 c.Curve(
     drawing=d,
-    start=(start_major[0] + copy_offset[0], start_major[1] + copy_offset[1]),
+    start=(start_minor[0] + copy_offset[0], start_minor[1] + copy_offset[1]),
     end=(
-        start_major[0] + copy_offset[0] + size[0],
-        start_major[1] + copy_offset[1] + size[1],
+        start_minor[0] + copy_offset[0] + size[0],
+        start_minor[1] + copy_offset[1] + size[1],
     ),
     offset=offset,
     height_offset=0,
