0f906faf9a91bce1aac5388c0c643221206f4436
sedatDemiriz <sedatdemiriz97@gmail.com>
2024-01-04 21:58:00 -0800

    feat: move looping logic to class

--- logo.py
+++ logo.py
@@ -29,6 +29,7 @@ class Curve:
         offset: tuple,
         translate: tuple,
         scale: tuple,
+        height_offset: int,
         color: str,
         debug: bool,
     ):
@@ -38,21 +39,25 @@ class Curve:
         self.color = color
         self.translate = translate
         self.scale = scale
+        self.height_repeats = 50
+        self.height_offset = height_offset
         self.stroke_width = 2
         self.debug = debug
 
         self.handle1 = (self.start[0] - self.offset[0], self.start[1] - self.offset[1])
         self.handle2 = (self.end[0] + self.offset[0], self.end[1] + self.offset[1])
 
-        self.path = dw.Path(
-            stroke=self.color,
-            fill="none",
-            stroke_width=self.stroke_width,
-            transform=f"translate({self.translate[0]}, {self.translate[1]}) scale({self.scale[0]}, {self.scale[1]})",
-        )
-        self.path.M(*self.start).C(*self.handle1, *self.handle2, *self.end)
-
-        drawing.append(self.path)
+        for i in range(self.height_repeats):
+            self.path = dw.Path(
+                stroke=self.color,
+                fill="none",
+                stroke_width=self.stroke_width,
+                transform=f"\
+                    translate(0, {i + self.height_offset}) \
+                    scale({self.scale[0]}, {self.scale[1]})",
+            )
+            self.path.M(*self.start).C(*self.handle1, *self.handle2, *self.end)
+            drawing.append(self.path)
 
         if self.debug:
             drawing.append(
